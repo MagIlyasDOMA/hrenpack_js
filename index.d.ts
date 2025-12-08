273ab97d68199a068140e98b81264d7c5d7a4893
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-12-08 18:19:10 +0300

    2.0.3

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export * from './arraywork';
+export * from './auto';
+export * from './classes';
+export * from './compiler';
+export * from './cookie';
+export * from './exceptions';
+export * from './get_element_types';
+export * from './html';
+export * from './input';
+export * from './link';
+export * from './notifications';
+export * from './styles';
+export * from './system';
+export * from './tags';
+export * from './url';
+export * from './uuid';
+//# sourceMappingURL=index.d.ts.map
