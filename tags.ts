253279778925e31f74235817457ae9fc76205b05
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2025-12-20 22:06:11 +0300

    3.1.5

--- tags.ts
+++ tags.ts
@@ -1,5 +1,5 @@
 /*
-* hrenpack_js 3.1.4
+* hrenpack_js 3.1.5
 * Copyright (c) 2024-2025, Маг Ильяс DOMA (MagIlyasDOMA)
 * Licensed under MIT (https://github.com/MagIlyasDOMA/hrenpack_js/blob/main/LICENSE)
 */
