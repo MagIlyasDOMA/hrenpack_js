563e5bf675352ed7b1291ba8d69fa180c4b28036
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-01-03 21:10:50 +0300

    3.4.1

--- types.d.ts
+++ types.d.ts
@@ -10,4 +10,8 @@ type NumStr = number | string;
 type NullBool = boolean | null;
 
 type OnOffType = 'on' | 'off';
+
+type Optional<T> = T | null
+type Nullable<T> = T | null | undefined
+
 //# sourceMappingURL=types.d.ts.map
